057bdad4ca92adbc0183eadccc1f5f38cd4efb7e
Sborresch <109110034+Sborresch@users.noreply.github.com>
2022-11-05 17:19:26 -0600

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,3 +12,19 @@ lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clea
 
 # Deliverable 1 Step 6 - Summary Function
 summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mecha_car))
+
+#Deliverable 2 Step 2
+suspension_coil <- read.csv(file='Suspension_Coil.csv')
+
+#Deliverable 2 Step 3
+total_summary <- suspension_coil %>% group_by() %>% summarize(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI), SD=sd(PSI))
+
+#Deliverable 2 Step 4 
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+
+
+
+
+
+
