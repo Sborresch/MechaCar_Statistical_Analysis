07ce2d4a1fd23c7e2c85c68c99da7057b29c1d2e
Sborresch <109110034+Sborresch@users.noreply.github.com>
2022-11-05 19:20:17 -0600

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -23,16 +23,16 @@ total_summary <- suspension_coil %>% group_by() %>% summarize(Mean=mean(PSI), Me
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
 
 #Deliverable 3 Step 1
-t.test(log10(lot_summary$Mean), mu=mean(log10(total_summary$Mean)))
+t.test(suspension_coil$PSI, mu=1500)
 
 #Deliverable 3 Step 2 - Lot 1
-t.test(log10(lot_summary$Mean), mu=mean(log10(total_summary$Mean)), subset(Manufactoring_Lot$Lot1))
+t.test(subset(suspension_coil, Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
 
-t.test(log10(subset(lot_summary$Manufacturing_Lot$Lot1)), mu=mean(log10(total_summary$Mean)))
-       
 #Deliverable 3 Step 2 - Lot 2
+t.test(subset(suspension_coil, Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
 
 #Deliverable 3 Step 2 - Lot 3
+t.test(subset(suspension_coil, Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
 
 
 
